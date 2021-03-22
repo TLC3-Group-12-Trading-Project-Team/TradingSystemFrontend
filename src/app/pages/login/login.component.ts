486492a496da7978467917e8923eb74a294e35a5
Kelvin Morrison <33485727+kelvinninja1@users.noreply.github.com>
2021-03-22 11:26:49 +0000

    Added: Login functionality

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { HttpClient } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-login',
@@ -6,11 +10,32 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit, OnDestroy {
-  constructor() {}
+  form: FormGroup;
 
+  constructor(
+    public fb: FormBuilder,
+    private http: HttpClient
+  ) {
+    this.form = this.fb.group({
+      email: [''],
+      password: ['']
+    })
+  }
   ngOnInit() {
   }
   ngOnDestroy() {
+  }
+
+  submitForm() {
+    // console.log("Submitted");
+    var formData: any = new FormData();
+    formData.append("email", this.form.get('email').value);
+    formData.append("password", this.form.get('password').value);
+
+    this.http.post('http://18.193.123.168:25000', formData).subscribe(
+      (response) => console.log(response),
+      (error) => console.log(error)
+    )
   }
 
 }

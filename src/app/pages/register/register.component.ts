2ced2247c1c7ecde2e68d1218877c11ce2301a1e
Kelvin Morrison <33485727+kelvinninja1@users.noreply.github.com>
2021-03-26 11:54:35 +0000

    Added: Register Functionality

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,10 +1,7 @@
-
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
-
-
 
 @Component({
   selector: 'app-register',
@@ -20,7 +17,8 @@ export class RegisterComponent implements OnInit, OnDestroy {
     private router: Router
   ) {
     this.form = this.fb.group({
-      name: [''],
+      firstname: [''],
+      lastname: [''],
       email: [''],
       password: ['']
     });
@@ -32,31 +30,48 @@ export class RegisterComponent implements OnInit, OnDestroy {
 
   CreateAccount() {
     // console.log("Submitted");
-    const formData: any = new FormData();
-    formData.append('email', this.form.get('email').value);
-    formData.append('firstname', this.form.get('firstname').value);
-    formData.append('lastname', this.form.get('lastname').value);
-    formData.append('password', this.form.get('password').value);
-
-    this.http.post('http://18.159.170.1:25000', formData).subscribe(
+     this.http.post('http://18.159.170.1:25000/client/signup', { firstname: this.form.get('firstname').value, lastname: this.form.get('lastname').value, email: this.form.get('email').value, password: this.form.get('password').value }).subscribe(
       (response) => this.signupAction(response),
       (error) => this.promptOnError(error),
-
     );
+
   }
 
   promptOnError(error) {
     console.log(error);
-    this.router.navigate(['dashboard']); // Replace with toast
+    alert("Failed To Sign Up");
+    // this.router.navigate(['dashboard']); // Replace with toast
   }
 
   signupAction(response) {
-    console.log(response);
-    // Check status code if Successful 200
-    // Find a way to store info
-
-    // redirect to dashboard
-    this.router.navigate(['login']);
+    console.log(response);``
+    // register client
+    this.http.post('http://18.159.170.1:25000/client/login', { email: this.form.get('email').value, password: this.form.get('password').value }).subscribe(
+      (response) => this.loginAction(response),
+      (error) => this.router.navigate(['login']),
+    );
   }
+
+  loginAction(response) {
+    //Check status code if Successful 200
+    console.log(response.code);
+    if(response.code == 200){
+      //Find a way to store info
+      console.log(response)
+
+      let key='userData';
+      localStorage.setItem(key, JSON.stringify(response));
+
+      // let getLocalDataItem = JSON.parse(localStorage.getItem(key));
+      // console.log(getLocalDataItem);
+
+      //redirect to dashboard
+      this.router.navigate(['dashboard']);
+    } else {
+      this.promptOnError(response);
+    }
+
+  }
+
 
 }

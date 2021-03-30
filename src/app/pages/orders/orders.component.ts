97937f2b19f38eaf2c0e9cdd2701900f114d7957
Kelvin Morrison <33485727+kelvinninja1@users.noreply.github.com>
2021-03-30 21:15:48 +0000

    Added: Orders and Portfolio CRUD

--- src/app/pages/orders/orders.component.ts
+++ src/app/pages/orders/orders.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import Chart from 'chart.js';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
 
 // core components
 import {
@@ -8,12 +10,16 @@ import {
   chartExample1,
   chartExample2
 } from "../../variables/charts";
+import {FormBuilder} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './orders.component.html',
   styleUrls: ['./orders.component.scss']
 })
+
 export class OrdersComponent implements OnInit {
 
   public datasets: any;
@@ -22,6 +28,42 @@ export class OrdersComponent implements OnInit {
   public clicked: boolean = true;
   public clicked1: boolean = false;
   public copy: string;
+  public UserData = JSON.parse(localStorage.getItem('userData'));
+
+  closeResult = '';
+
+
+  public activePortfolio = this.UserData.portfolios[0];
+  public activePortfolioStocks = this.UserData.portfolios[1].orders;
+
+  constructor(private modalService: NgbModal, private http: HttpClient, private router: Router) {}
+
+  open(orderContent) {
+    this.modalService.open(orderContent, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+  public newOrder = {
+    portfolioId: 0,
+    product: "",
+    quantity: 0,
+    price: 0.0,
+    side: "BUY"
+  }
+  public portfolios = this.UserData.portfolios;
 
   ngOnInit() {
 
@@ -31,8 +73,11 @@ export class OrdersComponent implements OnInit {
     ];
     this.data = this.datasets[0];
 
-
+    this.getPortfolios();
     var chartOrders = document.getElementById('chart-orders');
+
+    this.getPortfolios();
+    this.getPortfoliosStocks(this.UserData.portfolios[0].id);
 
     parseOptions(Chart, chartOptions());
 
@@ -58,4 +103,44 @@ export class OrdersComponent implements OnInit {
     this.salesChart.update();
   }
 
+  getPortfolios(){
+    // console.log("Submitted");
+    this.http.get('http://18.159.170.1:25000/portfolio/'+this.UserData.id).subscribe(
+      (response) => this.portfolios=response,
+      (error) => this.promptOnError(error)
+    );
+  }
+
+  getPortfoliosStocks(id: any) {
+    // console.log(id);
+    this.http.get('http://18.159.170.1:25000/portfolio/single/' + id).subscribe(
+      (response) =>  this.activePortfolioStocks= response,
+      (error) => this.promptOnError(error)
+    );
+
+  }
+
+
+  recommendNewOrderPrice() {
+
+  }
+
+  createNewOrder() {
+    // console.log("Submitted");
+    this.http.post('http://18.159.170.1:25000/order/getOrderStatus', { portfolioId: this.newOrder.portfolioId, product: this.newOrder.product, quantity: this.newOrder.quantity, price: this.newOrder.price, side: this.newOrder.side, action: "create" }).subscribe(
+      (response) => this.successAction(response),
+      (error) => this.promptOnError(error)
+    );
+  }
+
+  private successAction(response) {
+    console.log(response)
+    alert("New Order made Successfully")
+    this.router.navigate([this.router.url]);
+  }
+
+  private promptOnError(error) {
+    console.log(error)
+    alert("New Order could not be completed please check internet access");
+  }
 }
--- src/app/pages/portfolios/portfolios.component.ts
+++ src/app/pages/portfolios/portfolios.component.ts
@@ -9,6 +9,10 @@ import {
   chartExample2
 } from "../../variables/charts";
 
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './portfolios.component.html',
@@ -22,6 +26,38 @@ export class PortfoliosComponent implements OnInit {
   public clicked: boolean = true;
   public clicked1: boolean = false;
 
+  public UserData = JSON.parse(localStorage.getItem('userData'));
+
+  public portfolios = this.UserData.portfolios;
+  public activePortfolio = this.UserData.portfolios[0];
+  public activePortfolioStocks = this.UserData.portfolios[1].orders;
+
+  public newPortfolio = {
+    name: "",
+    clientEmail: this.UserData.email
+  }
+
+  constructor(private modalService: NgbModal, private http: HttpClient, private router: Router) {}
+
+  closeResult = '';
+  open(portfolioContent) {
+    this.modalService.open(portfolioContent, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
   ngOnInit() {
 
     this.datasets = [
@@ -30,7 +66,11 @@ export class PortfoliosComponent implements OnInit {
     ];
     this.data = this.datasets[0];
 
+    this.getPortfolios();
+    this.getPortfoliosStocks(this.UserData.portfolios[0].id);
 
+    // console.log(this.activePortfolio);
+    // console.log(this.activePortfolioStocks);
     var chartOrders = document.getElementById('chart-orders');
 
     parseOptions(Chart, chartOptions());
@@ -57,4 +97,39 @@ export class PortfoliosComponent implements OnInit {
     this.salesChart.update();
   }
 
+  getPortfolios(){
+    // console.log("Submitted");
+    this.http.get('http://18.159.170.1:25000/portfolio/'+this.UserData.id).subscribe(
+      (response) => this.portfolios=response,
+      (error) => this.promptOnError(error)
+    );
+  }
+
+  private promptOnError(error) {
+    alert("Error Occurred, You might be having connectivity issues")
+  }
+
+  createNewPortfolio() {
+    // console.log("Submitted");
+    this.http.post('http://18.159.170.1:25000/portfolio/create', { clientEmail: this.newPortfolio.clientEmail, name: this.newPortfolio.name}).subscribe(
+      (response) => this.successAction(response),
+      (error) => this.promptOnError(error)
+    );
+  }
+
+  private successAction(response) {
+    // console.log(response)
+    alert("New Portfolio made Successfully")
+    this.getPortfolios();
+  }
+
+
+  getPortfoliosStocks(id: any) {
+    // console.log(id);
+    this.http.get('http://18.159.170.1:25000/portfolio/single/' + id).subscribe(
+      (response) =>  this.activePortfolioStocks= response,
+      (error) => this.promptOnError(error)
+    );
+
+  }
 }
